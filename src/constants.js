c1f7d1a96ff0ce18e471466a863f6fd457621df4
JeanYoungPark <jean.young.park.96@gmail.com>
2024-04-30 14:47:45 +0900

    style: 폰트, 소개 수정

--- src/constants.js
+++ src/constants.js
@@ -2,15 +2,15 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-      I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-      Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+      I also like React and Php. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/JeanYoungPark" target="_blank">Github</a>!`,
     "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube.
     I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
     You'll learn how I built this portfolio you're currently playing through!`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+     <a href="https://www.youtube.com/@freecodecamp" target="_blank">freecodecamp</a>, <a href="https://www.youtube.com/@codingapple" target="_blank">코딩애플</a>,
+    <a href="https://www.youtube.com/@nomadcoders" target="_blank">nomadcoders</a> and <a href="https://www.youtube.com/@devbadak" target="_blank">개발바닥</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
     resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
     Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
@@ -19,5 +19,5 @@ export const dialogueData = {
     Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
     library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+    exit: `If you want to exit JeanYoung's portfolio, just close the tab.`,
 };
